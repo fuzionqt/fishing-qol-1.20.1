b27858cdf21390e541b8d3ebeecd50bf1022aaea
FUZION <fuzionplaysrbx@gmail.com>
2026-01-20 13:27:56 -0500

    Initial commit

---
+++ src/main/java/net/fuzion/fishingqol/FishingQOLClient.java
@@ -0,0 +1,10 @@
+package net.fuzion.fishingqol;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingQOLClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
